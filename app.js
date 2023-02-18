0f427d99a5480e3372ed19296853a4f36fd0811a
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2023-02-18 12:50:40 +0100

    allowedoringins

--- app.js
+++ app.js
@@ -19,6 +19,19 @@ const allowedOrigins = [
 
 app.use(cors({ origin: allowedOrigins }));
 
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", "*");
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
 app.use(express.json());
 
 app.post("/signin", login);
