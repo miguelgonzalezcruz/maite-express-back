8ac8dfd30a1952a9e1febf5331b0f3e6d8716ec2
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2023-02-19 21:44:54 +0100

    all origins

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ mongoose.connect("mongodb://localhost:27017/maite_db");
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://maiteapp.students.nomoredomainssbs.ru",
-  "https://www.maiteapp.students.nomoredomainssbs.ru",
-  "http://www.maiteapp.students.nomoredomainssbs.ru",
-  "http://maiteapp.students.nomoredomainssbs.ru",
-  "https://api.maiteapp.students.nomoredomainssbs.ru",
-  "http://api.maiteapp.students.nomoredomainssbs.ru",
-];
+// const allowedOrigins = [
+//   "http://localhost:3000",
+//   "https://maiteapp.students.nomoredomainssbs.ru",
+//   "https://www.maiteapp.students.nomoredomainssbs.ru",
+//   "http://www.maiteapp.students.nomoredomainssbs.ru",
+//   "http://maiteapp.students.nomoredomainssbs.ru",
+//   "https://api.maiteapp.students.nomoredomainssbs.ru",
+//   "http://api.maiteapp.students.nomoredomainssbs.ru",
+// ];
 
-app.use(cors({ origin: allowedOrigins }));
+app.use(cors({ origin: "*" }));
 
 app.use(express.json());
 
