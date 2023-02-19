fa61c35f319891e29b25d5240fa9b2fddeb98045
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2023-02-19 21:32:06 +0100

    cors

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "https://maiteapp.students.nomoredomainssbs.ru",
+  "https://www.maiteapp.students.nomoredomainssbs.ru",
+  "http://www.maiteapp.students.nomoredomainssbs.ru",
   "http://maiteapp.students.nomoredomainssbs.ru",
   "https://api.maiteapp.students.nomoredomainssbs.ru",
   "http://api.maiteapp.students.nomoredomainssbs.ru",
