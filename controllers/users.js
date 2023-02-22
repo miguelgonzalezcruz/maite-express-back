50421223bf5c921b772e16012ae05b8a4c1daa59
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2023-02-22 20:33:18 +0100

    hubspot

--- controllers/users.js
+++ controllers/users.js
@@ -9,75 +9,75 @@ const Boom = require("boom");
 const User = require("../models/user");
 const { errorHandling, orFailError } = require("../utils/errors");
 
-const HubsK = process.env.HubsK;
+// const HubsK = process.env.HubsK;
 // const JWT_SECRET = process.env.JWT_SECRET;
 
-const { JWT_SECRET } = require("../utils/config");
+const { JWT_SECRET, HubsK } = require("../utils/config");
 
 const hubspotClient = new hubspot.Client({
   accessToken: HubsK,
 });
 
-// const createUser = async (req, res) => {
-//   const { name, surname, email, phone, typeofuser, password } = req.body;
+const createUser = async (req, res) => {
+  const { name, surname, email, phone, typeofuser, password } = req.body;
 
-//   try {
-//     const user = await User.findOne({ email });
-//     if (user) {
-//       throw Boom.badRequest("User already exists");
-//     }
-
-//     const hash = await bcrypt.hash(password, 10);
-
-//     const newUser = await User.create({
-//       name,
-//       surname,
-//       email,
-//       phone,
-//       typeofuser,
-//       password: hash,
-//     });
-
-//     await hubspotClient.crm.contacts.basicApi.create({
-//       properties: {
-//         email: email,
-//         firstname: name,
-//         lastname: surname,
-//         phone: phone,
-//       },
-//     });
-
-//     res.status(201).send(newUser);
-//   } catch (error) {
-//     if (Boom.isBoom(error)) {
-//       res.status(error.output.statusCode).json(error.output.payload);
-//     } else {
-//       errorHandling(error, res);
-//     }
-//   }
-// };
-
-const createUser = (req, res) => {
-  const { name, surname, email, phone, typeofuser } = req.body;
-  User.findOne({ email }).then((user) => {
+  try {
+    const user = await User.findOne({ email });
     if (user) {
-      return res.status(400).send({ message: "User already exists" });
+      throw Boom.badRequest("User already exists");
     }
-    return bcrypt.hash(req.body.password, 10).then((hash) => {
-      User.create({ name, surname, email, phone, typeofuser, password: hash })
-        .then((data) => {
-          res.status(201).send(data);
-        })
-        .catch((err) => {
-          if (Boom.isBoom(err)) {
-            res.status(err.output.statusCode).json(err.output.payload);
-          } else {
-            errorHandling(err, res);
-          }
-        });
+
+    const hash = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      name,
+      surname,
+      email,
+      phone,
+      typeofuser,
+      password: hash,
     });
-  });
+
+    await hubspotClient.crm.contacts.basicApi.create({
+      properties: {
+        email: email,
+        firstname: name,
+        lastname: surname,
+        phone: phone,
+      },
+    });
+
+    res.status(201).send(newUser);
+  } catch (error) {
+    if (Boom.isBoom(error)) {
+      res.status(error.output.statusCode).json(error.output.payload);
+    } else {
+      errorHandling(error, res);
+    }
+  }
 };
+
+// const createUser = (req, res) => {
+//   const { name, surname, email, phone, typeofuser } = req.body;
+//   User.findOne({ email }).then((user) => {
+//     if (user) {
+//       return res.status(400).send({ message: "User already exists" });
+//     }
+//     return bcrypt.hash(req.body.password, 10).then((hash) => {
+//       User.create({ name, surname, email, phone, typeofuser, password: hash })
+//         .then((data) => {
+//           res.status(201).send(data);
+//         })
+//         .catch((err) => {
+//           if (Boom.isBoom(err)) {
+//             res.status(err.output.statusCode).json(err.output.payload);
+//           } else {
+//             errorHandling(err, res);
+//           }
+//         });
+//     });
+//   });
+// };
 
 const login = (req, res) => {
   const { email, password } = req.body;
--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,5 @@
 const JWT_SECRET = "some-secret key";
 
-module.exports = { JWT_SECRET };
+const HubsK = "pat-eu1-e2e4571b-76a7-4b3d-b4af-796234dd6e76";
+
+module.exports = { JWT_SECRET, HubsK };
