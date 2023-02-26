951b8c304feb0a4dfc8b9e5d0f1c8bcc50f2b315
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2023-02-26 20:50:57 +0100

    validation

---
+++ Validation/uservalidation.js
@@ -0,0 +1,25 @@
+const { Joi } = require("celebrate");
+
+const createUserSchema = Joi.object({
+  email: Joi.string().required().email(),
+  password: Joi.string().required().min(8),
+  name: Joi.string().required().min(2).max(30),
+  surname: Joi.string().required().min(2).max(30),
+  phone: Joi.string().required().min(5).max(11),
+  typeofuser: Joi.string().required().min(2).max(30),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().required().email(),
+  password: Joi.string().required().min(8),
+});
+
+const getCurrentUserSchema = Joi.object({
+  email: Joi.string().required().email(),
+  name: Joi.string().required().min(2).max(30),
+  surname: Joi.string().required().min(2).max(30),
+  phone: Joi.string().required().min(5).max(11),
+  typeofuser: Joi.string().required().min(2).max(30),
+});
+
+module.exports = { createUserSchema, loginSchema, getCurrentUserSchema };
